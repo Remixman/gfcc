f15604a998a54f2c11fea90f3bb861094c29f303
Remixman <haoremixman@gmail.com>
2013-06-03 16:32:00 +0700

    add more test

---
+++ griffon_tests/01_ni_pi/pi_num.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <gfn.h>
+
+long long get_time() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000000) + tv.tv_usec;
+}
+
+double integrate(int n) {
+	double sum, h, x;
+	int i;
+	h = 1.0 / (double) n;
+	#pragma gfn parallel_for reduction(+:sum) input(h,n)
+	for (i = 1; i <= n; i++) {
+		x = h*((double)i - 0.5);
+		sum += 4.0 / (1.0 + x*x);
+	}
+	return sum * h;
+}
+
+int main(int argc, char *argv[]) {
+	int n, ite, i, elapse;
+	double PI25DT = 3.14159265358;
+	double pi;
+
+	n = 2000000;
+	ite = 10;
+
+	if (argc > 1) n = atoi(argv[1]);
+	if (argc > 2) ite = atoi(argv[2]);
+	
+	#pragma gfn start
+  
+	// warm up
+	pi = integrate(n);
+
+	time0 = get_time();
+	for(i = 0; i < ite; i++) pi = integrate(n);
+	time1 = get_time();
+		
+	printf("last pi=%.16f, error=%.16f\n", pi, fabs(pi - PI25DT));
+	
+	#pragma gfn finish
+
+	printf("TEST 01 - Numerical Integration PI\n");
+	printf("\tProblem size N = %d\n", n);
+	printf("\tRun iteration = %d\n", ite);
+	printf("\tAverage time = %d sec.\n", ((float)(time1-time0)/1000000)/ite);
+	
+	return 0;
+}
+
---
+++ griffon_tests/02_matmul/matmul.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <gfn.h>
+
+#define N 500
+
+int n = N;
+float A[N][N]; 
+float B[N][N]; 
+float C[N][N];
+
+long long get_time() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000000) + tv.tv_usec;
+}
+
+void init() {
+	int i, j;
+	
+	srand(time(NULL));
+	
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			/* Random from 0.00 - 0.99 */
+			A[i][j] = (rand() % 100) / 100.f;
+			B[i][j] = (rand() % 100) / 100.f;
+		}
+	}
+}
+
+void matmul_kernel() {
+	int i, j, k;
+	
+	#pragma gfn parallel_for input(A,B) output(C) size(A,B,C:n,n)
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			C[i][j] = 0.f;
+			for (k = 0; k < n; k++) {
+				C[i][j] += A[i][k] * B[k][j];
+			}
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
+	int ite, i;
+	long long time0, time1;
+
+	ite = 10;
+
+	if (argc > 1) n = atoi(argv[1]);
+	if (argc > 2) ite = atoi(argv[2]);
+	
+	#pragma gfn start
+	
+	init();
+  
+	// warm up
+	matmul_kernel();
+
+	time0 = get_time();
+	for(i = 0; i < ite; i++) matmul_kernel();
+	time1 = get_time();
+	
+	#pragma gfn finish
+
+	printf("TEST 02 - Matrix Matrix Multiplication\n");
+	printf("\tProblem size = %d x %d\n", n, n);
+	printf("\tRun iteration = %d\n", ite);
+	printf("\tAverage time = %f sec.\n", ((float)(time1-time0)/1000000)/ite);
+	
+	return 0;
+}
+
