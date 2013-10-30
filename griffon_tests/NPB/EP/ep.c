b1691070f9aa5756b58e013864903655ccba6abc
Remixman <haoremixman@gmail.com>
2013-10-30 21:04:54 +0700

    add npbparams for EP's S class

--- griffon_tests/NPB/EP/ep.c
+++ griffon_tests/NPB/EP/ep.c
@@ -1,5 +1,28 @@
 
 #include "npb-C.h"
+#include "npbparams.h"
+
+/* parameters */
+#define	MK		16
+#define	MM		(M - MK)
+#define	NN		(1 << MM)
+#define	NK		(1 << MK)
+#define	NQ		10
+#define EPSILON		1.0e-8
+#define	A		1220703125.0
+#define	S		271828183.0
+
+#if defined(USE_POW)
+#define r23 pow(0.5, 23.0)
+#define r46 (r23*r23)
+#define t23 pow(2.0, 23.0)
+#define t46 (t23*t23)
+#else
+#define r23 (0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5*0.5)
+#define r46 (r23*r23)
+#define t23 (2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0*2.0)
+#define t46 (t23*t23)
+#endif
 
 int main(int argc, char **argv) {
 
@@ -11,7 +34,22 @@ int main(int argc, char **argv) {
     boolean verified;
     char size[13+1];	/* character*13 */
 
+/*
+c   Because the size of the problem is too large to store in a 32-bit
+c   integer for some classes, we put it into a string (for printing).
+c   Have to strip off the decimal point put in there by the floating
+c   point print statement (internal file)
+*/
 
+    printf("\n\n NAS Parallel Benchmarks 2.3 OpenACC C version"
+	   " - EP Benchmark\n");
+    sprintf(size, "%12.0f", pow(2.0, M+1));
+    for (j = 13; j >= 1; j--) {
+	if (size[j] == '.') size[j] = ' ';
+    }
+    printf(" Number of random numbers generated: %13s\n", size);
+
+    verified = FALSE;
     return 0;
 }
 
---
+++ griffon_tests/NPB/EP/npbparams.h
@@ -0,0 +1,18 @@
+/* CLASS = S */
+/*
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class of the NPB
+c  in this directory. Do not modify it by hand.
+*/
+#define	CLASS	 'S'
+#define	M	24
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "30 Oct 2013"
+#define NPBVERSION "2.3"
+#define CS1 "gcc"
+#define CS2 "gcc"
+#define CS3 "(none)"
+#define CS4 "-I../common"
+#define CS5 "-O3 "
+#define CS6 "(none)"
+#define CS7 "randdp"
